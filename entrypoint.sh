f4d5a440956cab058b821feaebcaa84ab80a0e69
Jocelyn Aladin <aladinjocelyn@gmail.com>
2022-06-27 00:02:20 -0400

    fixe some errors and typos

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,2 @@
 echo -n "AWS for Fluent Bit Container Image Version "
-exec /fluent-bit/bin/fluent-bit -e /fluent-bit/firehose.so -e /fluent-bit/cloudwatch.so -e /fluent-bit/kinesis.so /fluent-bit/out_grafana_loki.so -c /fluent-bit/etc/fluent-bit.conf
+exec /fluent-bit/bin/fluent-bit -e /fluent-bit/firehose.so -e /fluent-bit/cloudwatch.so -e /fluent-bit/kinesis.so -e /fluent-bit/out_grafana_loki.so -c /fluent-bit/etc/fluent-bit.conf
