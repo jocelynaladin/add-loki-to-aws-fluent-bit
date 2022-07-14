e633b0daafa5d02a966c90be897b73b9a527ad12
Jocelyn Aladin <jocelyn@mediavine.com>
2022-07-14 12:42:11 -0400

    trying mutiline logs

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,3 @@
+#!/bin/bash
 echo -n "AWS for Fluent Bit Container Image Version "
-exec /fluent-bit/bin/fluent-bit -e /fluent-bit/firehose.so -e /fluent-bit/cloudwatch.so -e /fluent-bit/kinesis.so -e /fluent-bit/out_grafana_loki.so -c /fluent-bit/etc/fluent-bit.conf
+exec /fluent-bit/bin/fluent-bit -e /fluent-bit/firehose.so -e /fluent-bit/cloudwatch.so -e /fluent-bit/kinesis.so -c /fluent-bit/etc/fluent-bit.conf
